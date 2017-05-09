064d304bec5c4608d20d906ae87633220fc8d9e7
TheDreadNought <oliver.obrius@gmail.com>
2017-05-09 14:48:10 +0200

    Denker Rombout, Doener Oliver. Story 04: UI klasse aangepast, Status werkend.

--- GamesuiteHangman/src/ui/Ui.java
+++ GamesuiteHangman/src/ui/Ui.java
@@ -2,7 +2,9 @@ package ui;
 
 import javax.swing.JOptionPane;
 
+import domain.Cirkel;
 import domain.Punt;
+import domain.Rechthoek;
 
 public class Ui {
 	public static void main(String[] args) {
@@ -12,6 +14,37 @@ public class Ui {
 		int newY = Integer.parseInt(y);
 		Punt punt = new Punt(newX, newY);
 		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
-
+		Object[] shapes ={"Cirkel","Rechthoek"};
+		String keuze = (String) JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
+		Object o = null;
+		switch(keuze){
+		case "Cirkel":
+			try {
+				String middelpuntX = JOptionPane.showInputDialog(" x coordinaat van het middelpunt : ");
+				String middelpuntY = JOptionPane.showInputDialog(" y coordinaat van het middlepunt : ");
+				
+				Punt puntCirkel = null;
+				int radius = Integer.parseInt(JOptionPane.showInputDialog("Wat is de raduis"));
+				 puntCirkel = new Punt(Integer.parseInt(middelpuntX), Integer.parseInt(middelpuntY));
+				 o= new Cirkel(puntCirkel,radius);
+				
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+			break;
+		case "Rechthoek":
+			try {
+				int hoekpuntX = Integer.parseInt(JOptionPane.showInputDialog(" x coordinaat van de linkerbovenhoek : "));
+				int hoekpuntY = Integer.parseInt(JOptionPane.showInputDialog(" y coordinaat van de linkerbovenhoek : "));
+				int breedte = Integer.parseInt(JOptionPane.showInputDialog(" breedte van de rechthoek : "));
+				int hoogte = Integer.parseInt(JOptionPane.showInputDialog(" hoogte van de rechthoek : "));
+				o= new Rechthoek(new Punt(hoekpuntX,hoekpuntY),breedte, hoogte);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			break;
+		}
+		System.out.println(o.toString());
 	}
 }
